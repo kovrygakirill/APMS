f9cf5610ddf91a510e15e122a979d93f60448fbb
Kovryga Kirill <kovrygakirill@mail.ru>
2021-04-16 16:50:25 +0300

    change model field of Client

--- client/models.py
+++ client/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Client(models.Model):
-    title = models.CharField(max_length=50, verbose_name='Название компании')
+    title = models.CharField(max_length=50, verbose_name='Name of company')
     description = models.TextField()
 
     def __str__(self):
