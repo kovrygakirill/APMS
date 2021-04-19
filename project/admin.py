fa88279181f56a893322399d9892d84721724b5a
Kovryga Kirill <kovrygakirill@mail.ru>
2021-04-19 16:33:42 +0300

    register modele of payment and project  in admin

--- project/admin.py
+++ project/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from project.models import Project, Task, CommentTask
 
-# Register your models here.
+admin.site.register(Project)
+admin.site.register(Task)
+admin.site.register(CommentTask)
