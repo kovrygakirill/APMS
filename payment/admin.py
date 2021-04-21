71f512daf71634497ce17d0a98b4c558cdc1dd91
Kovryga Kirill <kovrygakirill@mail.ru>
2021-04-21 18:09:45 +0300

    create base class of payment

--- payment/admin.py
+++ payment/admin.py
@@ -1,23 +1,23 @@
 from django.contrib import admin
-from django.utils.html import format_html
-from django.urls import reverse
 
 from payment.models import Payment
 from admin_numeric_filter.admin import RangeNumericFilter
 from client.admin import BaseModelAdmin
 
 
-class PaymentAdmin(BaseModelAdmin):
-    fields = ['comment', 'price', 'project']
-    search_fields = ['project__title']
-    ordering = ['project']
-    list_display = ['comment', 'get_price_in_dollar', 'get_project', 'default_actions']
-    list_filter = ('project', ('price', RangeNumericFilter), )
-
+class BasePaymentAdmin:
     def get_price_in_dollar(self, obj):
         return f'{obj.price} $'
 
     get_price_in_dollar.short_description = 'price'
+
+
+class PaymentAdmin(BaseModelAdmin, BasePaymentAdmin):
+    fields = ['comment', 'price', 'project']
+    search_fields = ['project__title']
+    ordering = ['project']
+    list_display = ['comment', 'get_price_in_dollar', 'get_project', 'default_actions']
+    list_filter = ('project', ('price', RangeNumericFilter),)
 
     def get_project(self, obj):
         return self.get_link_to_obj(obj.project)
