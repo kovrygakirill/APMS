ad0b1c5c83994b89c9c4a5e8135d8ee80ebf05a5
Kovryga Kirill <kovrygakirill@mail.ru>
2021-05-13 23:02:37 +0300

    update user profile admin

--- user_profile/admin.py
+++ user_profile/admin.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from .models import UserProfile
 from project.admin import BaseModelAdmin
+from project.admin import TaskInstanceInline
 
 
 class UserProfileAdmin(BaseModelAdmin):
-    # readonly_fields = ('username', 'email', 'first_name', 'last_name')
+    inlines = [TaskInstanceInline]
+    fields = ('username', 'email', 'first_name', 'last_name')
     list_display = ['username', 'email', 'first_name', 'last_name', 'default_actions']
     list_filter = ['is_superuser', ]
     search_fields = ['username', 'email', 'first_name', 'last_name']
