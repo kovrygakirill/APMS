6250f295005c3ba7348c5e8540f0dd0802c78540
Kovryga Kirill <kovrygakirill@mail.ru>
2021-05-16 15:47:08 +0300

    refactoring User Profile in admin

--- user_profile/admin.py
+++ user_profile/admin.py
@@ -4,8 +4,13 @@ from project.admin import BaseModelAdmin
 from project.admin import TaskInstanceInline
 
 
+class TaskInstanceInlineProxy(TaskInstanceInline):
+    fields = ['get_task_link', 'get_start_datetime', 'get_finish_datetime', 'get_total_time', 'type', 'status', ]
+    readonly_fields = ['get_task_link', 'get_start_datetime', 'get_finish_datetime', 'get_total_time', 'type', 'status']
+
+
 class UserProfileAdmin(BaseModelAdmin):
-    inlines = [TaskInstanceInline]
+    inlines = [TaskInstanceInlineProxy]
     fields = ('username', 'email', 'first_name', 'last_name')
     list_display = ['username', 'email', 'first_name', 'last_name', 'default_actions']
     list_filter = ['is_superuser', ]
